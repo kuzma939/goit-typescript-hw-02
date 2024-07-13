1818e39367e2455d81a5556c24fda963943e8092
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-07-13 13:50:06 +0200

    script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "@typescript-eslint/no-explicit-any": "error",
     "react/prop-types": 0,
     "react/jsx-no-target-blank": "off",
+    
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,3 +1,4 @@
+
 import styles from './LoadMoreBtn.module.css';
 
 interface LoadMoreBtnProps {
